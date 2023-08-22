11d5afdaaa5bf4aeaac707120fe1cf3ce79cc58f
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-08-22 16:57:06 +0530

    pdate

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { categoriesData, productData } from "../../static/data";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+
 import { BiMenuAltLeft } from "react-icons/bi";
 import DropDown from "./DropDown";
 import Navbar from "./Navbar";
@@ -121,17 +123,50 @@ const Header = ({activeHeading}) => {
                   setDropDown={setDropDown}
                 />
               ) : null}
-
-            
             </div>
-            
           </div>
-            {/* navitems */}
-            <div className={`${styles.noramlFlex}`}>
-                <Navbar active={activeHeading}/>
-              </div>
+          {/* navitems */}
+          <div className={`${styles.noramlFlex}`}>
+            <Navbar active={activeHeading} />
+          </div>
 
-              
+          <div className="flex">
+            <div className={`${styles.noramlFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                <AiOutlineHeart
+                  size={30}
+                  color="rgb(255 255 255 /83%)"
+                />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                  0
+                </span>
+              </div>
+            </div>
+            <div className={`${styles.noramlFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                <AiOutlineShoppingCart
+                  size={30}
+                  color="rgb(255 255 255 /83%)"
+                />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                  0
+                </span>
+              </div>
+            </div>
+            <div className={`${styles.noramlFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                <CgProfile
+                  size={30}
+                  color="rgb(255 255 255 /83%)"
+                />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                  0
+                </span>
+              </div>
+            </div>
+          </div>
+
+     
         </div>
       </div>
     </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Layout/Header'
+import Hero from '../components/Route/Hero/Hero'
 
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1}/>
+      <Hero/>
     </div>
   )
 }
