b16c059e81b68dcc458cb62e72fac5b275943232
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-08-25 11:57:37 +0530

    update-categories

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -159,9 +159,7 @@ const Header = ({activeHeading}) => {
                   size={30}
                   color="rgb(255 255 255 /83%)"
                 />
-                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
-                  0
-                </span>
+               
               </div>
             </div>
           </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Layout/Header'
 import Hero from '../components/Route/Hero/Hero'
+import Categories from '../components/Route/Categories/Categories'
 
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1}/>
       <Hero/>
+      <Categories/>
     </div>
   )
 }
