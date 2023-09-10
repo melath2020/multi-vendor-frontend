9ba9910afbaa45d0c6ed19817ffa44949a291553
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-09-10 12:48:25 +0530

    updte

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from '../components/Login/Login.jsx';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if(isAuthenticated === true){
+      navigate("/");
+    }
+  }, [])
+
   return (
     <div >
       <Login/>
