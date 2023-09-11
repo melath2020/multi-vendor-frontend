0146639109580cba5f153fcb399bda86deddb20e
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-09-11 21:21:00 +0530

    updatecart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,56 +1,98 @@
-import React from 'react'
-import { RxCross1 } from 'react-icons/rx'
-import styles from '../../styles/styles'
+import React, { useState } from "react";
+import { RxCross1 } from "react-icons/rx";
+import styles from "../../styles/styles";
 import { IoBagHandleOutline } from "react-icons/io5";
+import { HiOutlineMinus, HiPlus } from "react-icons/hi";
 
-const Cart = ({setOpenCart}) => {
-    const cartData=[
-        {
-            name :"Iphone 14 Pro max ssd and 8gb ram silver color",
-            description:"test",
-            price :999,
-        },
-        {
-            name :"Iphone 14 Pro max ssd and 8gb ram silver color",
-            description:"test",
-            price :245,
-        },
-        {
-            name :"Iphone 14 Pro max ssd and 8gb ram silver color",
-            description:"test",
-            price :324,
-        },
-    ]
+const Cart = ({ setOpenCart }) => {
+  const cartData = [
+    {
+      name: "Iphone 14 Pro max ssd and 8gb ram silver color",
+      description: "test",
+      price: 999,
+    },
+    {
+      name: "Iphone 14 Pro max ssd and 8gb ram silver color",
+      description: "test",
+      price: 245,
+    },
+    {
+      name: "Iphone 14 Pro max ssd and 8gb ram silver color",
+      description: "test",
+      price: 324,
+    },
+  ];
   return (
-    <div className='fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10'>
-         <div className="fixed top-0 right-0 h-full w-[80%] 800px:w-[25%] bg-white flex flex-col overflow-y-scroll justify-between shadow-sm">
-            <div className="flex w-full justify-end pt-5 pr-5">
-                <RxCross1 size={25} className='cursor-pointer' onClick={()=>setOpenCart(false)}/>
-            </div>
-            
-            {/* items length */}
-            <div className={`${styles.noramlFlex} p-4`}>
-                <IoBagHandleOutline size={25} />
-                <h5 className="pl-2 text-[20px] font-[500]">
-                   items
-                </h5>
-              </div>
-
-               {/* cart Single Items */}
-               <br />
-              <div className="w-full border-t">
-                {
-                cartData && cartData.map((i,index)=>(
-                    <>hi</>
-                ))
-                }
-              </div>
-           
-
-
+    <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
+      <div className="fixed top-0 right-0 h-full w-[80%] 800px:w-[25%] bg-white flex flex-col overflow-y-scroll justify-between shadow-sm">
+        <div className="flex w-full justify-end pt-5 pr-5">
+          <RxCross1
+            size={25}
+            className="cursor-pointer"
+            onClick={() => setOpenCart(false)}
+          />
         </div>
-    </div>
-  )
-}
 
-export default Cart
+        {/* items length */}
+        <div className={`${styles.noramlFlex} p-4`}>
+          <IoBagHandleOutline size={25} />
+          <h5 className="pl-2 text-[20px] font-[500]">items</h5>
+        </div>
+
+        {/* cart Single Items */}
+        <br />
+        <div className="w-full border-t">
+          {cartData &&
+            cartData.map((i, index) => <CartSingle key={index} data={i} />)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CartSingle = ({ data }) => {
+  const [value, setValue] = useState(1);
+  const totalPrice = data.price * value;
+  return (
+    <div className="border-b p-4">
+      <div className="w-full flex items-center">
+        <div>
+          <div
+            className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.noramlFlex} justify-center cursor-pointer`}
+            onClick={() => setValue(value + 1)}
+          >
+            <HiPlus size={18} color="#fff" />
+          </div>
+          <span className="pl-[10px]">{value}</span>
+          <div
+            className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
+            onClick={() => setValue(value === 1 ? 1 : value - 1)}
+          >
+            <HiOutlineMinus size={16} color="#7d879c" />
+          </div>
+        </div>
+        <img
+          src='https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204'
+          alt=""
+          className="w-[130px] h-min ml-2 mr-2 rounded-[5px]"
+        />
+ <div className="pl-[5px]">
+          <h1>Mob redmi note 6</h1>
+          <h4 className="font-[400] text-[15px] text-[#00000082]">
+            $34
+          </h4>
+          <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
+            US$58
+          </h4>
+        </div>
+        <RxCross1
+          className="cursor-pointer"
+          
+        />
+    
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
