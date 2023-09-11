2c3838a37bee98e0303b01338ffca9ad411c36b2
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2023-09-11 20:25:12 +0530

    update_cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { RxCross1 } from 'react-icons/rx'
+import styles from '../../styles/styles'
+import { IoBagHandleOutline } from "react-icons/io5";
 
 const Cart = ({setOpenCart}) => {
     const cartData=[
@@ -25,8 +27,27 @@ const Cart = ({setOpenCart}) => {
             <div className="flex w-full justify-end pt-5 pr-5">
                 <RxCross1 size={25} className='cursor-pointer' onClick={()=>setOpenCart(false)}/>
             </div>
-            {/* items length */}
             
+            {/* items length */}
+            <div className={`${styles.noramlFlex} p-4`}>
+                <IoBagHandleOutline size={25} />
+                <h5 className="pl-2 text-[20px] font-[500]">
+                   items
+                </h5>
+              </div>
+
+               {/* cart Single Items */}
+               <br />
+              <div className="w-full border-t">
+                {
+                cartData && cartData.map((i,index)=>(
+                    <>hi</>
+                ))
+                }
+              </div>
+           
+
+
         </div>
     </div>
   )
